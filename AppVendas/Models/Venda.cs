e2ecf576b1fd178fb6e9f231d3e465029e5d900a
BiaS1lva6 <beatriz080407@gmail.com>
2024-09-11 16:57:50 -0300

    Criação do cadastro de vendas

---
+++ AppVendas/Models/Venda.cs
@@ -0,0 +1,15 @@
+﻿namespace AppVendas.Models
+{
+    public class Venda
+    {
+        public Guid VendaId { get; set; }
+        public int NotaFiscal { get; set; }
+        public DateTime DataVenda { get; set; }
+        public double? TotalVenda { get; set; }
+
+        /*Referencia para a model cliente*/
+        public Guid ClientesId { get; set; }
+        public Cliente? Cliente { get; set; }
+
+    }
+}
